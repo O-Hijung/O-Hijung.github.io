b9bc6e1fb4740eea7e998762417e7c9594aec7be
O-Hijung <godls9712@gmail.com>
2022-10-10 02:29:55 +0900

    edit storage

--- js/apiKey.js
+++
@@ -1,3 +0,0 @@
-const globalVarApi = {
-    key: "b6e7508916944709e333e076db123400",
-}
--- js/inputValue.js
+++
@@ -1,6 +0,0 @@
-const loginInput = document.querySelector("input");
-const loginButton = document.querySelector("button");
-
-function getInputValue() {
-    const userName = loginInput.value;
-}
